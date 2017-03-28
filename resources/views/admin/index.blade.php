80823392e40407b49a9635cb9172af66e4b14990
MrHuang <hoangluckyboy@icloud.com>
2017-03-28 21:40:37 +0800

    validator验证服务
    此节需要多看看

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.admin')
 @section('content')
-	<!--头部 开始-->
-	<div class="top_box">
-		<div class="top_left">
-			<div class="logo">后台管理模板</div>
-			<ul>
-				<li><a href="#" class="active">首页</a></li>
-				<li><a href="#">管理页</a></li>
-			</ul>
-		</div>
-		<div class="top_right">
-			<ul>
-				<li>管理员：admin</li>
-				<li><a href="pass.html" target="main">修改密码</a></li>
-				<li><a href="{{url('admin/quit')}}">退出</a></li>
-			</ul>
-		</div>
+		<!--头部 开始-->
+<div class="top_box">
+	<div class="top_left">
+		<div class="logo">后台管理模板</div>
+		<ul>
+			<li><a href="#" class="active">首页</a></li>
+			<li><a href="#">管理页</a></li>
+		</ul>
 	</div>
-	<!--头部 结束-->
+	<div class="top_right">
+		<ul>
+			<li>管理员：admin</li>
+			<li><a href="{{url('admin/pass')}}" target="main">修改密码</a></li>
+			<li><a href="{{url('admin/quit')}}">退出</a></li>
+		</ul>
+	</div>
+</div>
+<!--头部 结束-->
 
 	<!--左侧导航 开始-->
 	<div class="menu_box">

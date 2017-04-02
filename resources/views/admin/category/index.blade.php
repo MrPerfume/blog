f1d0556723d9015dd7ab770750906269951e402a
MrHuang <hoangluckyboy@icloud.com>
2017-04-02 20:37:28 +0800

    文章的修改
    注意下框架的put提交需要隐藏字段发送

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -29,19 +29,21 @@
 {{--</div>--}}
 {{--<!--结果页快捷搜索框 结束-->--}}
 
-    <!--搜索结果页面 列表 开始-->
-    <form action="#" method="post">
-        <div class="result_wrap">
-            <!--快捷导航 开始-->
-            <div class="result_content">
-                <div class="short_wrap">
-                    <a href="#"><i class="fa fa-plus"></i>新增文章</a>
-                    <a href="#"><i class="fa fa-recycle"></i>批量删除</a>
-                    <a href="#"><i class="fa fa-refresh"></i>更新排序</a>
-                </div>
-            </div>
-            <!--快捷导航 结束-->
+<!--搜索结果页面 列表 开始-->
+<form action="#" method="post">
+    <div class="result_wrap">
+        <div class="result_title">
+            <h3>分类管理</h3>
         </div>
+        <!--快捷导航 开始-->
+        <div class="result_content">
+            <div class="short_wrap">
+                <a href="{{url('admin/category/create')}}"><i class="fa fa-plus"></i>添加分类</a>
+                <a href="{{url('admin/category')}}"><i class="fa fa-recycle"></i>全部分类</a>
+            </div>
+        </div>
+        <!--快捷导航 结束-->
+    </div>
 
     <div class="result_wrap">
         <div class="result_content">
@@ -67,8 +69,8 @@
                     <td>{{$v->cate_title}}</td>
                     <td>{{$v->cate_view}}</td>
                     <td>
-                        <a href="#">修改</a>
-                        <a href="#">删除</a>
+                        <a href="{{url('admin/category/'.$v->cate_id.'/edit')}}">修改</a>
+                        <a href="javascript:;" onclick="delCate({{$v->cate_id}})">删除</a>
                     </td>
                 </tr>
                 @endforeach
